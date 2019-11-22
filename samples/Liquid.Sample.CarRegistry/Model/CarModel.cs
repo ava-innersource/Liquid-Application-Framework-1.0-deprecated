2ba2b64e58ac9ca802a529898bab2acb62d161cd
bruno.brant <bruno.brant@outlook.com>
2019-11-22 17:55:19 -0300

    feat: adds a small sample on how to use Liquid

---
+++ samples/Liquid.Sample.CarRegistry/Model/CarModel.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using Liquid.Repository;
+using System;
+
+namespace Liquid.Sample.CarRegistry
+{
+    /// <summary>
+    /// Describes a Car 
+    /// </summary>
+    public class Car : LightModel<Car>
+    {
+        /// <summary>
+        /// Identifies the car
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Id of the car in the legacy system
+        /// </summary>
+        public string IdLegacy { get; set; }
+
+        /// <summary>
+        /// Describes the car object
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <inheritdoc/>
+        public override void Validate()
+        {
+            RuleFor(i => i.Id).NotEmpty().WithErrorCode("ID_MUSTNOT_BE_EMPTY");
+        }
+    }
+}
---
+++ samples/Liquid.Sample.CarRegistry/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Liquid.Sample.CarRegistry
+{
+#pragma warning disable S1118
+    /// <summary>
+    /// Class Program
+    /// </summary> 
+    public class Program
+#pragma warning restore S1118
+    {
+        /// <summary>
+        /// Main class of project
+        /// </summary>
+        /// <param name="args">args</param>
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        /// <summary>
+        /// Method to build a web host
+        /// </summary>
+        /// <param name="args">args</param>
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
