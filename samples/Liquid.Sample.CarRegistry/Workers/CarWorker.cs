14fa0a40657f59ecf324666b39c61d3471ed1c1a
bruno.brant <bruno.brant@outlook.com>
2019-11-22 17:55:19 -0300

    feat: adds a small sample on how to use Liquid

---
+++ samples/Liquid.Sample.CarRegistry/Workers/CarWorker.cs
@@ -0,0 +1,25 @@
+﻿using Liquid.Activation;
+
+namespace Liquid.Sample.CarRegistry
+{
+    /// <summary>
+    /// Worker of Car
+    /// </summary>
+    [MessageBus("BUS")]
+    public class CarWorker : LightWorker
+    {
+        /// <summary>
+        /// Method Get Message
+        /// </summary>
+        /// <param name="message">CarMessage Object</param>
+        [Topic("car", "LegacyCarSubs", 10, false)]
+        public void GetMessage(CarMessage message)
+        {
+            ValidateInput(message);
+            //Calls domain (business) logic
+            var response = Factory<CarService>().SaveWorker(message.LegacyCarVM);
+            //Terminates the message according to domain response
+            Terminate(response.Result);
+        }
+    }
+}
