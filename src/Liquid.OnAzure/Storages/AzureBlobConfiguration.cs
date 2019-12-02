cd7c924aaca0aa93e0b1559dc2cd54c0c2a84623
Bruno Brant <1890900+bruno-brant@users.noreply.github.com>
2019-12-02 15:04:23 -0300

    feat(MediaStorage): adds support for SAS
    Added support to Shared Access Signature for ILightMediaStorage.

---
+++ src/Liquid.OnAzure/Storages/AzureBlobConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Liquid.Runtime.Configuration
+{
+    public class AzureBlobConfiguration : LightConfig<AzureBlobConfiguration>
+    {
+        public string ConnectionString { get; set; }
+        public string Container { get; set; }
+
+        public override void Validate()
+        {
+            RuleFor(d => ConnectionString).NotEmpty().WithMessage("'ConnectionString' on AzureBlob settings should not be empty.");
+
+            RuleFor(d => Container).NotEmpty().WithMessage("'Container' on AzureBlob settings should not be empty.");
+
+        }
+    }
+}
