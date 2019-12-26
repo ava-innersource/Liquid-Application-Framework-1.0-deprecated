cd8f773ffff5f7b139e434079897a80d4dfd8bea
bruno-brant <1890900+bruno-brant@users.noreply.github.com>
2019-12-26 11:00:49 -0300

    fix: test should be a fact
    Also adjusted some using clauses.

--- test/Liquid.OnAzure.Tests/AzureBlobTests.cs
+++ test/Liquid.OnAzure.Tests/AzureBlobTests.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
-using System.Globalization;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
