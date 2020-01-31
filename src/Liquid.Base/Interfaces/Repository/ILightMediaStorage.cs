d437ed2e596cec3d5547373e8b6a456d94a3e7ff
Bruno Brant <1890900+bruno-brant@users.noreply.github.com>
2020-01-31 11:15:15 -0300

    feat(AzureBlob): increases coverage  (#163)
    I achieved good coverage with most generic methods. Surely there are more situations we can test, but this is a initial harness that will enable us to more confidently make changes to this class.
    
    Closes #166.

--- src/Liquid.Base/Interfaces/Repository/ILightMediaStorage.cs
+++ src/Liquid.Base/Interfaces/Repository/ILightMediaStorage.cs
@@ -1,18 +1,53 @@
-﻿using Liquid.Base.Interfaces;
+﻿// Copyright (c) Avanade Inc. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 using System;
 using System.Threading.Tasks;
+using Liquid.Base.Interfaces;
 
 namespace Liquid.Interfaces
 {
     /// <summary>
-    /// Public interface for all Media Storage Implementations 
+    /// Defines an object capable of storing arbritary media.
     /// </summary>
     public interface ILightMediaStorage : IWorkbenchHealthCheck
-    {	
-		string Conection { get; set; }
+    {
+        /// <summary>
+        /// Gets or sets the connection string for the storage provider.
+        /// </summary>
+        [Obsolete("This property will be removed in later version. Please refrain from accessing it.")]
+        string Connection { get; set; }
+
+        /// <summary>
+        /// Gets or sets the container name used to store data in the provider.
+        /// </summary>
+        [Obsolete("This property will be removed in later version. Please refrain from accessing it.")]
         string Container { get; set; }
+
+        /// <summary>
+        /// Gets an <see cref="ILightAttachment"/> from storage.
+        /// </summary>
+        /// <param name="resourceId">The directory of the container.</param>
+        /// <param name="id">The name of the media file.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        /// The task result contains an object that encapsulates access to the data in the container entry.
+        /// </returns>
+        /// <exception cref="Exception">Throws when the resource doesn't exists.</exception>
         Task<ILightAttachment> GetAsync(string resourceId, string id);
-        Task InsertUpdateAsync(ILightAttachment attachment); 
-        Task Remove(ILightAttachment attachment); 
+
+        /// <summary>
+        /// Upserts an <see cref="ILightAttachment"/> to the storage.
+        /// </summary>
+        /// <param name="attachment">Describes the data that will be stored/updated.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task InsertUpdateAsync(ILightAttachment attachment);
+
+        /// <summary>
+        /// Removes an item from the underlying storage.
+        /// </summary>
+        /// <param name="attachment">Describes the data that will be stored/updated.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task Remove(ILightAttachment attachment);
     }
 }
