0dea830c5c8494730503cbea2cb4fd22ee913b77
Andre Racz <andre.racz@avanade.com>
2020-02-05 10:07:31 -0300

    Improvements on SecretsConfiguration validation

--- src/Liquid.Runtime/Secrets/SecretsConfiguration.cs
+++ src/Liquid.Runtime/Secrets/SecretsConfiguration.cs
@@ -14,13 +14,14 @@ namespace Liquid.Runtime
 
         public override void Validate()
         {
-            RuleFor(d => Enable).NotNull().WithMessage("'Enable' on Secrets settings should not be empty.");
+            //Removed validation becaus Enable cannot be null
+            //RuleFor(d => Enable).NotNull().WithMessage("'{PropertyName}' on Secrets settings should not be empty.");
             
             //Validating parameters that do not come from body by a Fluent-like sintax
-            RuleFor(d => Module).NotEmpty().When(d => Enable).WithMessage("'Module' on Secrets settings should not be empty.");
-            RuleFor(d => Name).NotEmpty().When(d => Enable).WithMessage("'Name' on Secrets settings should not be empty.");
+            RuleFor(d => Module).NotEmpty().When(d => Enable).WithMessage("'{PropertyName}' on Secrets settings should not be empty.");
+            RuleFor(d => Name).NotEmpty().When(d => Enable).WithMessage("'{PropertyName}' on Secrets settings should not be empty.");
 
-            RuleFor(d => Module).Equal("K8S_SECRETS").When(d => Enable).WithMessage("'Module' on Secrets settings should be equal to K8S_SECRETS");
+            RuleFor(d => Module).Equal("K8S_SECRETS").When(d => Enable).WithMessage("'{PropertyName}' on Secrets settings should be equal to K8S_SECRETS");
 
         }
     }
