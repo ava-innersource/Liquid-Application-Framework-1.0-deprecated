3b777982040431569682f87476aa4ebe43d74c17
Bruno Brant <1890900+bruno-brant@users.noreply.github.com>
2020-02-10 22:10:26 -0300

    fix: ILightAttachment is disposable now (#175)
    Since ILightAttachment has a disposable member, it must also be disposable.
    
    I've also updated the implementation of ILightAttachment to correctly dispose of stream, but it falls to the developer to properly dispose of it. Let's hope the warning that the compiler gives is enough.
    
    Closes #33.

--- src/Liquid.Repository/LightAttachment.cs
+++ src/Liquid.Repository/LightAttachment.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Liquid.Interfaces;
 using Newtonsoft.Json;
 
@@ -18,5 +19,41 @@ namespace Liquid.Repository
         public virtual Stream MediaStream { get; set; }
         [JsonIgnore]
         public string MediaLink { get; set; }
+
+        /// <summary>
+        /// Destructor, calls disposing to free unmanaged resources.
+        /// </summary>
+        ~LightAttachment()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Releases unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Dipose of unmanaged resources.
+        /// </summary>
+        /// <param name="disposing">Whether this is being called from Dispose or from the GC.</param>
+        /// <remarks>
+        /// Implements the disposing pattern.
+        /// </remarks>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (MediaStream != null)
+                {
+                    MediaStream.Dispose();
+                    MediaStream = null;
+                }
+            }
+        }
     }
 } 
