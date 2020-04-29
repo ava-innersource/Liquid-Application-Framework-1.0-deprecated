0ca2aab03e33ab831f4c0bb789028571c24c56e0
Leila Ferreira <leila.costa.ferreira@avanade.com>
2020-04-29 16:52:24 -0300

    #199 creation of tests and bug fix

--- test/Liquid.OnAzure.Tests/AzureBlobIntegrationTests.cs
+++ test/Liquid.OnAzure.Tests/AzureBlobIntegrationTests.cs
@@ -18,9 +18,9 @@ using Microsoft.WindowsAzure.Storage.Blob;
 using NSubstitute;
 using Xunit;
 
-namespace Liquid.OnAzure.Tests
+namespace Liquid.OnAzure.IntegrationTests
 {
-    public class AzureBlobTests : IDisposable
+    public class AzureBlobIntegrationTests : IDisposable
     {
         private const string ContentType = "text/plain";
         private const string DefaultConnectionString = "UseDevelopmentStorage=true";
@@ -37,7 +37,7 @@ namespace Liquid.OnAzure.Tests
 
         private readonly AzureBlob _sut;
 
-        public AzureBlobTests()
+        public AzureBlobIntegrationTests()
         {
             Workbench.Instance.Reset();
 
---
+++ test/Liquid.OnAzure.Tests/MessageBuses/ServiceBusIntegrationTest.cs
@@ -0,0 +1,42 @@
+﻿using Liquid.Activation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Liquid.OnAzure.IntegrationTests.MessageBuses
+{
+    public class ServiceBusIntegrationTest : IDisposable
+    {
+        readonly ServiceBus _serviceBus;
+        
+        public ServiceBusIntegrationTest()
+        {
+            _serviceBus = new ServiceBus();
+
+        }
+
+        [Fact]
+        public void ServiceBusWhenThrowsException()
+        {
+            Assert.ThrowsAny<Exception>(() => _serviceBus.ProcessQueue());
+        }
+
+        [Fact]
+        public void InitializeWhenThrowsException()
+        {
+            Assert.ThrowsAny<Exception>(() => _serviceBus.Initialize());
+        }
+
+        [Fact]
+        public void ProcessSubscriptionWhenThrowsException()
+        {
+            Assert.ThrowsAny<Exception>(() => _serviceBus.ProcessSubscriptione());
+        }
+
+        public void Dispose()
+        {
+           
+        }
+    }
+}
